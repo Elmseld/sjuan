99a30417d33fef01a999dad1945ab2abac69b426
essob <aronzzon@hotmail.com>
2014-04-09 09:08:34 +0200

    Modifierade Controller klass

--- Controller.java
+++ Controller.java
@@ -1,9 +1,16 @@
 package sjuan;
 
-public class Controller {
+import java.util.ArrayList;
 
-	
+public class Controller {
+	private ArrayList <Card> cardDeck;
+
 	public Controller() {
+		
+	}
+	
+	//för test av controllern
+	public static void main (String[] args) {
 		
 	}
 }
