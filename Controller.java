3a7ef46a941ff92b8d9b06848abd8891694f7da5
essob <aronzzon@hotmail.com>
2014-04-14 21:47:09 +0200

    Created a Deck of cards and  Deal method to deal cards
    Card class updated. Controller class updated. Deck class created.
    Player class updated.

--- Controller.java
+++ Controller.java
@@ -1,51 +1,38 @@
 package sjuan;
-
-import java.util.ArrayList;
-import java.util.Random;
 /**
  * This class controlls the logic of the game "Sjuan"
  * @author Tobbe
  *
  */
 public class Controller {
-	private ArrayList<Card> cardDeck;
 	private Player player1, player2, player3, player4;
+	private Deck deck;
 
 	/**
-	 * Constructs a controller
+	 * Constructs a controller 
+	 * @param player1 takes in a player
+	 * @param player2 takes in a player
+	 * @param player3 takes in a player
+	 * @param player4 takes in a player
+	 * @param deck takes in a deck
 	 */
-	public Controller() {
-
+	public Controller(Player player1, Player player2, Player player3, Player player4, Deck deck) {
+		this.player1 = player1;
+		this.player2 = player2;
+		this.player3 = player3;
+		this.player4 = player4;
+		this.deck = deck;
 	}
 	/**
-	 * Sets card values in the cardDeck
+	 * This method deals the deck to all players
 	 */
-	public void setCardDeck() {
-		cardDeck = new ArrayList<Card>();
-		String[] values  = {
-				"s1","s2","s3","s4","s5","s6","s7","s8","s9","s10","sj","sd","sk",
-				"h1","h2","h3","h4","h5","h6","h7","h8","h9","h10","hj","hd","hk",
-				"d1","d2","d3","d4","d5","d6","d7","d8","d9","d10","dj","dd","dk",
-				"c1","c2","c3","c4","c5","c6","c7","c8","c9","c10","cj","cd","ck"};
-		for (int i = 0; i < values.length;i++) 
-			cardDeck.add(new Card(values[i]));
+	public void Deal() { 
+		while (deck.getAllCards()!=0) {
+			player1.setPlayerCards(deck.dealCard());
+			player2.setPlayerCards(deck.dealCard());
+			player3.setPlayerCards(deck.dealCard());
+			player4.setPlayerCards(deck.dealCard());
 
-		//		for(int i = 0; i<cardDeck.size(); i++) 
-		//			System.out.print(cardDeck.get(i).getValue() + ", ");  
-	}
-	// denna metod ska dela ut 13 kort till varje spelare
-	public void deal() {
-		Random rand = new Random();
-		rand.nextInt(51);
-		
-		
-
-	}
-
-	//för test av controllern
-	public static void main (String[] args) {
-		Controller control = new Controller();
-		control.setCardDeck();
-
+		}
 	}
 }
