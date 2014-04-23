95fd6dcbf0eaf56f1a321d9cf021c6cb7433d239
Anna Nordkvist <annanordkvist@yahoo.se>
2014-04-23 12:30:44 +0200

    lagt till DB-connection

--- DataBase.java
+++ DataBase.java
@@ -1,12 +1,54 @@
-/*
- * This Class is Cool
- */
-
 package sjuan;
+import java.sql.*;
 
 public class DataBase {
-	
-	public DataBase() {
-		
-	}
+    public static Connection connection;
+    public static Statement statement;
+    
+    public static void showResultSet(ResultSet resultSet) throws SQLException {
+        ResultSetMetaData meta = resultSet.getMetaData();
+        String res = "";
+
+        int colCount = meta.getColumnCount();
+        for(int i=1; i<=colCount; i++)
+            res += meta.getColumnLabel(i) + ", ";
+
+        res += "\n";
+
+        while(resultSet.next()) {
+            for(int i=1; i<=colCount; i++)
+                res += resultSet.getObject(i).toString() + ", ";
+
+            res += "\n";
+        }
+        System.out.println(res);
+    }
+    
+    public static void connect() throws SQLException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://195.178.232.7:4040/ab4607","ab4607","prinsessan");
+            statement = connection.createStatement();
+        } catch(ClassNotFoundException e1) {
+            System.out.println("Databas-driver hittades ej: "+e1);
+        }
+    }
+    
+    public static void disconnect() throws SQLException {
+        statement.close();
+        connection.close();
+    }
+    
+    public static void main(String[] args) {
+        try {
+            connect();
+            
+            ResultSet result = statement.executeQuery("SELECT name, points FROM ab4607.statistics");
+            showResultSet(result);
+            
+            disconnect();
+        } catch(SQLException e) {
+            System.out.println(e);
+        }
+    }
 }
